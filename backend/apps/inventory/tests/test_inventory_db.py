7e635a561c55570c39d4c862397486d3e574b8fa
perymerdeka <tkjasikk@gmail.com>
2022-07-28 23:04:35 +0700

    WIP: Add Factory

---
+++ backend/apps/inventory/tests/test_inventory_db.py
@@ -0,0 +1,36 @@
+import pytest
+from apps.inventory.models import InventoryModel, CategoryModel
+
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "id, name, slug, is_active",
+    [
+        (1, "fashion", "fashion", 1),
+        (18, "trainers", "trainers", 1),
+        (35, "baseball", "baseball", 1),
+    ],
+)
+def test_inventory_category_dbfixture(
+    db, db_fixture_setup, id, name, slug, is_active
+):
+    """
+    Test InventoryCategory DB Fixture
+    """
+    result = CategoryModel.objects.get(id=id)
+    result.name == name
+    result.slug == slug
+    result.is_active == is_active
+
+
+@pytest.mark.parametrize(
+    "slug, is_active",
+    [
+        ("fashion", 1),
+        ("trainers", 1),
+        ("baseball", 1),
+    ],
+)
+def test_inventory_db_category_insert_data(
+    db, category_factory, slug, is_active
+):
+    result = category_factory.create(slug=slug, is_active=is_active)
---
+++ backend/tests/factories.py
@@ -0,0 +1,5 @@
+import factory
+import pytest
+
+from faker import Faker
+from pytest_factoryboy import register
