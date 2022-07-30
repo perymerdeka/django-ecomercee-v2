79461c7150dd61027592f9c70cf8c29eb6104be7
perymerdeka <tkjasikk@gmail.com>
2022-07-30 19:27:20 +0700

    WIP: Category Testing

--- backend/apps/inventory/admin.py
+++ backend/apps/inventory/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from apps.inventory.models import InventoryModel, CategoryModel
 
 # Register your models here.
+
+
+class InventoryModelAdmin(admin.ModelAdmin):
+    pass
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    pass
+
+
+# register model admin
+
+admin.site.register(InventoryModel, InventoryModelAdmin)
+admin.site.register(CategoryModel, CategoryModelAdmin)
--- backend/apps/inventory/models.py
+++ backend/apps/inventory/models.py
@@ -1,11 +1,53 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey, TreeManyToManyField
 
 # Create your models here.
+
 
 class InventoryModel(models.Model):
     pass
 
-class CategoryModel(models.Model):
-    name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
+
+class CategoryModel(MPTTModel):
+    """
+    Inventory Category Model with MPTT
+    """
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name=_("category name"),
+        null=False,
+        blank=False,
+        unique=True,
+        help_text="format: required, max_length=100",
+    )
+    slug = models.SlugField(
+        max_length=100,
+        null=False,
+        blank=False,
+        verbose_name=_("category safe URL"),
+        help_text=_(
+            "format: required, letters, numbers, underscore, or hyphens"
+        ),
+    )
     is_active = models.BooleanField(default=True)
+    parent = TreeForeignKey(
+        "self",
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        related_name="children",
+        unique=False,
+        verbose_name=_("parent category"),
+        help_text=_("format:  not required"),
+    )
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+    class Meta:
+        verbose_name = _("product category")
+        verbose_name_plural = _("product categories")
+    
+    def __str__(self):
+        return self.name
